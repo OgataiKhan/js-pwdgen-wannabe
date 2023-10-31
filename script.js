f371c8fbd99c7b864085a5bee16ffd6c793d08c0
OgataiKhan <badellino.kevin@gmail.com>
2023-10-31 14:22:08 +0100

    feat: add console output

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ elementName.innerHTML = prompt('Qual è il tuo nome?')
 elementSurname.innerHTML = prompt('Qual è il tuo cognome?');
 elementColor.innerHTML = prompt('Qual è il tuo colore preferito?');
 elementNumber.innerHTML = Math.floor(Math.random() * (99 - 10 + 1)) + 10; // Genero un numero intero casuale compreso tra 10 e 99. In caso volessi cambiare il range è sufficiente cambiare questi due numeri nella formula.
+
+console.log(elementName.innerHTML);
+console.log(elementSurname.innerHTML);
+console.log(elementColor.innerHTML);
+console.log(elementNumber.innerHTML);
