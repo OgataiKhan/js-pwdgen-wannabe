695c79fba4540417ade8b2b5297cf1c62bc9ca4f
OgataiKhan <badellino.kevin@gmail.com>
2023-10-31 14:00:28 +0100

    feat: add script & define variables

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const elementName = document.getElementById('name');
+const elementSurname = document.getElementById('surname');
+const elementColor = document.getElementById('color');
+const elementNumber = document.getElementById('number');
+
