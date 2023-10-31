2e9ccdb07aa64416fe7ca7ad29e1b28a3bd474ca
OgataiKhan <badellino.kevin@gmail.com>
2023-10-31 14:17:53 +0100

    feat: add prompts & output on page

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const elementSurname = document.getElementById('surname');
 const elementColor = document.getElementById('color');
 const elementNumber = document.getElementById('number');
 
+elementName.innerHTML = prompt('Qual è il tuo nome?')
+elementSurname.innerHTML = prompt('Qual è il tuo cognome?');
+elementColor.innerHTML = prompt('Qual è il tuo colore preferito?');
+elementNumber.innerHTML = Math.floor(Math.random() * (99 - 10 + 1)) + 10; // Genero un numero intero casuale compreso tra 10 e 99. In caso volessi cambiare il range è sufficiente cambiare questi due numeri nella formula.
